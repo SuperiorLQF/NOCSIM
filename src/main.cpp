7e24b23b9a61fa91e73dbcc31eefae6d7f06dd64
SuperiorLQF <superiorlqf@gmail.com>
2025-10-26 18:35:58 +0800

    SystemC base environment

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,63 @@
 #include <systemc.h>
-#include "nand2.h"
-#include "tb.h"
-int sc_main(int, char * *){
-    sc_signal<bool>a,b,f;
-    sc_clock clk("Clk",40,SC_NS);
-    nand2 N2("Nand2");
-    N2.A(a);
-    N2.B(b);
-    N2.F(f);
-    tb tb1("tb");
-    tb1.clk(clk);
-    tb1.a(a);
-    tb1.b(b);
-    tb1.f(f);
+#include "tb.h"  
+#include "regslice.h"      // DUT
 
-    sc_trace_file *tf = sc_create_vcd_trace_file("Nand2");
-    sc_trace(tf,N2.A,"A");
-    sc_trace(tf,N2.B,"B");
-    sc_trace(tf,N2.F,"F");
-    sc_trace(tf,clk,"clk");
-    sc_start(30000,SC_NS);
+int sc_main(int argc, char* argv[]) {
+
+    // 信号定义
+    sc_clock        clk("clk", 10, SC_NS);     // 100 MHz
+    sc_signal<bool> rst_n;
+
+    sc_signal<bool> mst_vld;
+    sc_signal<bool> mst_rdy;
+    sc_signal<int>  mst_payload;
+
+    sc_signal<bool> slv_vld;
+    sc_signal<bool> slv_rdy;
+    sc_signal<int>  slv_payload;
+
+    // 实例化 DUT
+    regslice<int> u_dut("u_dut");
+    u_dut.clk(clk);
+    u_dut.rst_n(rst_n);
+    u_dut.mst_vld(mst_vld);
+    u_dut.mst_rdy(mst_rdy);
+    u_dut.mst_payload(mst_payload);
+    u_dut.slv_vld(slv_vld);
+    u_dut.slv_rdy(slv_rdy);
+    u_dut.slv_payload(slv_payload);
+
+    // 实例化 TBench
+    tb_regslice tb("tb");
+    tb.clk(clk);
+    tb.rst_n(rst_n);
+    tb.mst_vld(mst_vld);
+    tb.mst_rdy(mst_rdy);
+    tb.mst_payload(mst_payload);
+    tb.slv_vld(slv_vld);
+    tb.slv_rdy(slv_rdy);
+    tb.slv_payload(slv_payload);
+
+    // ====== 波形 trace ======
+    sc_trace_file* tf = sc_create_vcd_trace_file("regslice_wave");
+    tf->set_time_unit(1, SC_NS);
+
+    sc_trace(tf, clk,         "clk");
+    sc_trace(tf, rst_n,       "rst_n");
+    sc_trace(tf, mst_vld,     "mst_vld");
+    sc_trace(tf, mst_rdy,     "mst_rdy");
+    sc_trace(tf, mst_payload, "mst_payload");
+    sc_trace(tf, slv_vld,     "slv_vld");
+    sc_trace(tf, slv_rdy,     "slv_rdy");
+    sc_trace(tf, slv_payload, "slv_payload");
+
+    // ========== 启动仿真 ==========
+    cout << "=== Simulation Start ===" << endl;
+    sc_start();
+    cout << "=== Simulation End   ===" << endl;
+   
     sc_close_vcd_trace_file(tf);
+
     return 0;
 
 }
--- src/tb.h
+++ src/tb.h
@@ -1,25 +1,90 @@
-#ifndef _TB_H
-#define _TB_H
-#include<systemc.h>
-SC_MODULE(tb){
-    sc_out<bool>a,b;
-    sc_in<bool>f;
-    sc_in_clk clk;
-    void gen_input(){
-        wait();a=0;b=0;
-        wait();a=0;b=1;
-        wait();a=1;b=0;
-        wait();a=1;b=1;    
-        wait(100);    
+#ifndef _TBRS_H
+#define _TBRS_H
+#include <systemc.h>
+
+SC_MODULE(tb_regslice) {
+    // 信号
+    sc_in_clk       clk;
+    sc_out<bool>    rst_n;
+
+    sc_out<bool> mst_vld;
+    sc_in<bool>  mst_rdy;
+    sc_out<int>  mst_payload;
+
+    sc_in<bool>  slv_vld;
+    sc_out<bool> slv_rdy;
+    sc_in<int>   slv_payload;
+
+
+    void master_proc();
+    void slave_proc();
+    void reset_proc();
+    void sim_time_ctrl();
+    
+    SC_CTOR(tb_regslice)
+    {
+        SC_THREAD(reset_proc);
+
+        SC_CTHREAD(sim_time_ctrl,clk.pos());
+        reset_signal_is(rst_n,false);        
+
+        SC_CTHREAD(master_proc,clk.pos());
+        reset_signal_is(rst_n,false);
+
+        SC_CTHREAD(slave_proc,clk.pos());
+        reset_signal_is(rst_n,false);
+
     }
-    void display_variable(){
-        cout<<"a="<<a<<",b="<<b<<",f="<<f<<endl;
-    }
-    SC_CTOR(tb){
-        SC_CTHREAD(gen_input,clk.pos());
-        SC_METHOD(display_variable);
-        sensitive<<f<<a<<b;
-        dont_initialize();
-    }
+
+
 };
+void tb_regslice::sim_time_ctrl(){
+    wait(100);
+    sc_stop();
+}
+void tb_regslice::reset_proc() {
+    rst_n.write(0);
+    wait(50, SC_NS);
+    rst_n.write(1);
+}
+// master 线程：驱动输入
+void tb_regslice::master_proc() {
+    mst_vld.write(false);
+    mst_payload.write(0);
+    wait();  // 等待一个上升沿
+
+    for (int i = 0; i < 10; i++) {
+        mst_payload.write(i);
+        mst_vld.write(true);
+        do {
+            wait(); // 等待直到 regslice 接受数据
+        } while (!mst_rdy.read());
+        cout << sc_time_stamp() << " [MASTER] send data = " << i << endl;
+        wait(); // 数据已推送成功
+
+    }
+
+    mst_vld.write(false);
+}
+
+// slave 线程：接受输出
+void tb_regslice::slave_proc() {
+    slv_rdy.write(false);
+
+    while (true) {
+
+        if (slv_vld.read() && slv_rdy.read()) {
+            cout << sc_time_stamp() << " [SLAVE ] got  data = " << slv_payload.read() << endl;
+        }
+
+        // 随机制造 backpressure
+        if (rand() % 4 == 0) {
+            slv_rdy.write(false);
+        } else {
+            slv_rdy.write(true);
+        }
+        wait();
+    }
+}
+
 #endif
